7911ae2b6a777c2636e4a54b157715a371258da5
AJ Boyd <alanjaredboyd3@yahoo.com>
2024-07-01 02:10:32 -0400

    added fresh new stylings and functionalities

--- eloquence/generate_content.py
+++ eloquence/generate_content.py
@@ -10,48 +10,56 @@ from langchain_core.callbacks import CallbackManager, StreamingStdOutCallbackHan
 from langchain_core.prompts import PromptTemplate
 import re, random
 
-SYS_PROMPT = "You are EloquenceAI, a tool designed to generate text-based content such as emails, essays, and fictional dialogue according to the user's prompt and preferences."
+SYS_PROMPT = "You are EloquenceAI, a tool designed to generate text-based content such as emails, stories, and fictional dialogue according to the user's prompt and preferences. All content must have proper grammar"
 
 def config_LLM():
     llm = Ollama(
         model = "llama3",
         verbose = False,
-        temperature = 1.7,
+        temperature = 1.3,
         system = SYS_PROMPT,
-        top_k = 50,
-        top_p = 0.95
+        top_k = 60,
+        top_p = 0.95,
+        num_predict = -2
     )
     
     return llm
 
 def gen_content(llm, attr: list) -> str:
-    _type, subject, length, style, tone, example = attr
+    _type, subject, model, length, style, tone, example = attr
     
     # update llm attributes depending on user preferences
-    if _type != "Essay":
-        if length == "Very brief" or _type == "Pick-up line":
-            llm.num_predict = 50
-        elif length == "Short":
-            llm.num_predict = 70
-        elif length == "Lengthy":
-            llm.num_predict = 300
-    else:
-        if length == "Very brief":
-            llm.num_predict = 70
-        elif length == "Short":
-            llm.num_predict = 120
-        elif length == "Lengthy":
-            llm.num_predict = 1100
+    llm.model = model
     
-    prompt = f"""Only respond with the content you generate. The user has asked you to generate a {length} {_type} that is {llm.num_predict - 20} or fewer words long. The {_type} must follow this subject prompt: [{subject}]
-    Make your writing style {style}"""
+    if "Story" == _type or "AITA" in _type:
+        if length == "Very brief":
+            approx_length = 70
+        elif length == "Short":
+            approx_length = 120
+        elif length == "Lengthy":
+            approx_length = 1050
+        else:
+            approx_length = 540
+    else:
+        if length == "Very brief" or _type == "Pick-up line":
+            approx_length = 70
+        elif length == "Short":
+            approx_length = 90
+        elif length == "Lengthy":
+            approx_length = 300
+        else:
+            approx_length = 150
+    
+    
+    prompt = f"""Only respond with the content you generate. The user has asked you to generate a {length} {_type} that is {approx_length - 40} or fewer words long. The {_type} is to be centered around this general subject: [{subject}]
+    Make your writing style {style}. """
     
     if tone:
-        prompt += f" and keep your tone {tone}"
+        prompt += f"Keep your tone {tone}. "
     if example:
-        prompt += f". Use this as an example <{example}>"
+        prompt += f"Use this as an example <{example}> "
     
-    prompt += "for e-mails, do not include a subject line."
+    prompt += "Do not include a subject line or title unless prompted otherwise."
     print("prompt:", prompt)
     response = llm.invoke(prompt)
     print("response:", response)
--- eloquence/static/script.js
+++ eloquence/static/script.js
@@ -1,10 +1,14 @@
 //special form submssion code
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function(){
     const form = document.getElementById('gen-form');
+    const content_container = document.getElementById("content-container");
+    const content = document.getElementById("content")
 
-    form.addEventListener('submit', function(event) {
+    form.addEventListener('submit', function(event){
         event.preventDefault(); 
-        document.querySelector('.loading-spinner').style.display = 'block'; //start loading animation
+        document.getElementById('loading-spinner').style.display = 'block'; //start loading animation
+        content.innerHTML = ""; //clear content
+        content_container.style.display = "none";
 
         // get form data
         const formData = new FormData(form);
@@ -22,13 +26,14 @@ document.addEventListener('DOMContentLoaded', function() {
             
             //display generated content
             msg = data.content;
-            content_box = document.getElementById("content");
-            content_box.style.display = "block";
+            content_container.style.display = "block";
+            console.log(msg)
             typeWriter(0, msg)
         })
         .catch(error => {
             //if error
             document.getElementById('loading-spinner').style.display = 'none';
+            content_container.style.display = "block";
             alert('An error occurred: ' + error.message); //display error message
             msg = "Error: " + error.message;
             typeWriter(0, msg);
@@ -38,11 +43,40 @@ document.addEventListener('DOMContentLoaded', function() {
 
 const SPEED = 25;
 //modified typewriter text effect via W3 schools
-function typeWriter(i, text) {
+function typeWriter(i, text){
     if (i < text.length) {
-      document.getElementById("content").innerHTML += text.charAt(i);
-      setTimeout(function() {
+      content.innerHTML += text.charAt(i);
+      setTimeout(function(){
         typeWriter(++i, text) 
       }, SPEED);
     }
-  }
+}
+
+function copyContent(){
+    copyBtn = document.getElementById("copyContent")
+    const textToCopy = document.getElementById('content').textContent;
+    navigator.clipboard.writeText(textToCopy).then(function() {
+        copyBtn.innerHTML = "Copied!"
+        icon = document.createElement("i");
+        icon.classList = "fa-solid fa-copy";
+        setTimeout(function(){
+           changeElemText(copyBtn, 'Copy <i class="fa-solid fa-copy"></i>')
+        }, 3000)
+    }, function(err) {
+        alert('Failed to copy text: ', err);
+    });
+}
+
+function exportContent(){
+    const exportText = document.getElementById('content').textContent;
+    const blob = new Blob([exportText], { type: 'text/plain' });
+    const anchor = document.createElement('a');
+    anchor.href = URL.createObjectURL(blob);
+    anchor.download = 'eloquentAI_generation.txt';
+    anchor.click();
+    URL.revokeObjectURL(anchor.href);
+}
+
+function changeElemText(elem, text){
+    elem.innerHTML = text;
+}
